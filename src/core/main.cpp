0f30c47247ebcce22374d65a902c71e8adbebac9
kavros <akavroulakis@gmail.com>
2018-10-09 18:05:20 +0100

    removes old code

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -2,15 +2,8 @@
 #include "../../include/LandscapeGenerator.hpp"
 using namespace std;
 
-/*int main()
+int main()
 {
-	//TODO: verify that numbers are ok
-	// 0 < width,height<=2000  
-	//and landDistribution+waterDistribution  ==100
-	
-	LandscapeGenerator landscape(50,50,40,60);
-	landscape.GenerateData();
-        
-	cout<<"Hello World"<<endl;
+    cout<<"Hello World"<<endl;
 }
-*/
+

73b1c7917b4a7e36cad64d2bbab0b948595cffe3
kavros <akavroulakis@gmail.com>
2018-10-16 09:28:55 +0100

    adds classes without implementations

---
+++ include/LandscapeSquare.hpp
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   LandscapeSquare.hpp
+ * Author: alex
+ *
+ * Created on October 9, 2018, 8:54 PM
+ */
+
+#ifndef LANDSCAPESQUARE_HPP
+#define LANDSCAPESQUARE_HPP
+
+class LandscapeSquare {
+public:
+    LandscapeSquare();
+    LandscapeSquare(const LandscapeSquare& orig);
+    virtual ~LandscapeSquare();
+private:
+    bool   isWater;
+    double pumas;
+    double hares;
+};
+
+#endif /* LANDSCAPESQUARE_HPP */
+
---
+++ src/core/LandscapeSquare.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   LandscapeSquare.cpp
+ * Author: alex
+ * 
+ * Created on October 9, 2018, 8:54 PM
+ */
+
+#include "LandscapeSquare.hpp"
+
+LandscapeSquare::LandscapeSquare() {
+}
+
+LandscapeSquare::LandscapeSquare(const LandscapeSquare& orig) {
+}
+
+LandscapeSquare::~LandscapeSquare() {
+}
+

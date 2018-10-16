4e4cd900fb3a67812960caa39b3bbcd585886814
kavros <akavroulakis@gmail.com>
2018-10-16 23:09:32 +0100

    change code to run multiple simulations

--- src/core/LandscapeSimulation.cpp
+++ src/core/LandscapeSimulation.cpp
@@ -37,53 +37,59 @@ LandscapeSimulation::~LandscapeSimulation()
 void LandscapeSimulation::Run()
 {
     double haresNew,pumasNew;
-    
-    for(unsigned int i=0; i < landscape.GetWidth(); i++)
+    for(int k = 0; k < landscape.GetT(); k++)
     {
-        for(unsigned int j=0; j < landscape.GetHeight(); j++)
+        for( int i=0; i < landscape.GetWidth(); i++)
         {
-            //what happens when we call GetPumas(-1,0),getN is unimplemented
-            pumasNew=
-                    landscape.GetPumas(i,j)+ 
-                    landscape.GetDt()*
-                    ( 
-                           
-                        landscape.GetB()*landscape.GetHares(i,j)*landscape.GetPumas(i,j)-
-                        landscape.GetM()*landscape.GetPumas(i,j)+
-                        landscape.GetL()*( 
-                                           (    
-                                                landscape.GetPumas(i-1,j) +
-                                                landscape.GetPumas(i+1,j) +
-                                                landscape.GetPumas(i,j-1) +
-                                                landscape.GetPumas(i,j+1)
-                                           )-
-                                            landscape.GetN(i,j)*landscape.GetPumas(i,j)
-                                        )
-                    
-                    );
-            
-            haresNew=
-                    landscape.GetHares(i,j)+
-                    landscape.GetDt()*
-                    (
-                        landscape.GetR()*landscape.GetHares(i,j)-
-                        (landscape.GetA()*landscape.GetHares(i,j)*landscape.GetPumas(i,j))+
-                        landscape.GetK()*(
-                                            ( 
-                                                landscape.GetHares(i-1,j) +
-                                                landscape.GetHares(i+1,j) +
-                                                landscape.GetHares(i,j-1) +
-                                                landscape.GetHares(i,j+1)
-                                            )-
-                                            landscape.GetN(i,j)*landscape.GetHares(i,j) 
-                                        )
-                    );
-                    
-            
-        }
-        
-    }       
+            for( int j=0; j < landscape.GetHeight(); j++)
+            {
+                if(landscape.IsSquareWater(i,j) == true)
+                {
+                    continue;
+                }
+                //what happens when we call GetPumas(-1,0),getN is unimplemented
+                pumasNew=
+                        landscape.GetPumas(i,j)+ 
+                        landscape.GetDt()*
+                        ( 
+
+                            landscape.GetB()*landscape.GetHares(i,j)*landscape.GetPumas(i,j)-
+                            landscape.GetM()*landscape.GetPumas(i,j)+
+                            landscape.GetL()*( 
+                                               (    
+                                                    landscape.GetPumas(i-1,j) +
+                                                    landscape.GetPumas(i+1,j) +
+                                                    landscape.GetPumas(i,j-1) +
+                                                    landscape.GetPumas(i,j+1)
+                                               )-
+                                                landscape.GetN(i,j)*landscape.GetPumas(i,j)
+                                            )
+
+                        );
+
+                haresNew=
+                        landscape.GetHares(i,j)+
+                        landscape.GetDt()*
+                        (
+                            landscape.GetR()*landscape.GetHares(i,j)-
+                            (landscape.GetA()*landscape.GetHares(i,j)*landscape.GetPumas(i,j))+
+                            landscape.GetK()*(
+                                                ( 
+                                                    landscape.GetHares(i-1,j) +
+                                                    landscape.GetHares(i+1,j) +
+                                                    landscape.GetHares(i,j-1) +
+                                                    landscape.GetHares(i,j+1)
+                                                )-
+                                                landscape.GetN(i,j)*landscape.GetHares(i,j) 
+                                            )
+                        );
+
+                landscape.SetPumas(i,j,pumasNew);
+                landscape.SetHares(i,j,haresNew);
+            }
+
+        }  
+    }
     
-    cout<<"Run"<<endl;
 }
 

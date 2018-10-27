6b8bc018994549783928c68c8dd2d4caa5e606e0
kavros <akavroulakis@gmail.com>
2018-10-27 18:17:28 +0100

    add cppunit library and env scrips

---
+++ set-paths.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export CPLUS_INCLUDE_PATH=cppunit-1.13.2/home/include:$CPLUS_INCLUDE_PATH
+export LIBRARY_PATH=cppunit-1.13.2/home/lib:$LIBRARY_PATH
+export LD_LIBRARY_PATH=cppunit-1.13.2/home/lib:$LD_LIBRARY_PATH

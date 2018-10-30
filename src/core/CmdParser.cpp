33a357ef14ca83eb2dff559d92a4d2367fda66d9
kavros <akavroulakis@gmail.com>
2018-10-30 17:05:54 +0000

    adds cmd parser for main to a class

--- src/core/CmdParser.cpp
+++ src/core/CmdParser.cpp
@@ -1,22 +1,97 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   CmdParser.cpp
- * Author: alex
- * 
- * Created on October 9, 2018, 6:48 PM
- */
-
 #include "CmdParser.hpp"
+#include <exception>
+#include <stdexcept>
+#include <cstring>
+#include <iostream>
+#include <cstdlib>
+#include "../../include/Args.hpp"
+using namespace std;
+CmdParser::CmdParser(int argc, char** argv) 
+{
 
-CmdParser::CmdParser() {
+    if(argc <= 1)
+    {
+        throw std::invalid_argument( "No arguments passed, use -h for help.");
+    }
+
+    args::ArgumentParser parser("Command Parser","A program to simulate Puma and Hare populations across a grid.");
+    args::HelpFlag help(parser, "help", "Display this help prompt.", {'h', "help"});
+    args::ValueFlag<string> configFile(parser, "filename", "JSON-format configuration file for the simulation.",{'c', "config"});
+    args::ValueFlag<string> inputFile(parser,"filename","Landscape input file to perform the simulation on.",{'i',"input"});
+    args::ValueFlag<string> outputPrefix(parser,"prefix","Prefix for each generated output file.",{'p',"prefix"});
+
+    try
+    {
+        parser.ParseCLI(argc,argv);
+        SetConfigFilePath(configFile.Get());
+        SetInputFilePath(inputFile.Get());
+        SetOutputFilePrefix(outputPrefix.Get());
+
+
+    }
+    catch (args::Help)
+    {
+      cout << parser;
+      exit(-1);
+    }
+    catch (args::ParseError e)
+    {
+      cerr << e.what() << endl;
+      exit(-1);
+    }
+    catch (args::ValidationError e)
+    {
+      cerr << e.what() << endl;
+      exit(-1);
+    }
+}
+
+void CmdParser::SetInputFilePath(std::string inputFile)
+{
+    if(inputFile.empty())
+    {
+        throw std::invalid_argument("Input File name cannot be empty");
+    }
+    this->landscapeFilePath = inputFile;
+}
+
+void CmdParser::SetConfigFilePath(std::string configFilePath)
+{
+    if(configFilePath.empty() )
+    {
+        throw std::invalid_argument("Configuration file name cannot be empty.");
+    }
+    
+    if( configFilePath.find(".json") ==string::npos)
+    {
+        throw std::invalid_argument("Configuration file must be json.");
+    }
+    this->configFilePath = configFilePath;
+}
+
+void CmdParser::SetOutputFilePrefix(std::string outputFilePrefix)
+{
+    this->outputFilePrefix = outputFilePrefix;
+}
+
+std::string CmdParser::GetInputFilePath()
+{
+    return landscapeFilePath;
+}
+
+std::string CmdParser::GetConfigFilePath()
+{
+    return configFilePath;
+}
+
+std::string CmdParser::GetOutputFilePrefix()
+{
+    return outputFilePrefix;
 }
 
 
-CmdParser::~CmdParser() {
+CmdParser::~CmdParser() 
+{
+    
 }
 

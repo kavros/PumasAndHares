b31e0220f2c77f63b3814733c1905790a71d87b2
amced <s1331613@ed.ac.uk>
2018-11-01 12:30:52 +0000

    add getters and setters for the config generator

--- include/ConfigurationGenerator.hpp
+++ include/ConfigurationGenerator.hpp
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-/* 
+/*
  * File:   ConfigurationGenerator.hpp
  * Author: alex
  *
@@ -26,13 +26,52 @@ public:
     ConfigurationGenerator(int ac, char *av[]);
     bool WriteToFile(std::string fileName);
 
-    std::string fileName;
-    json options;
-    std::string jsonString;
-private:
+    double GetR();
+    int SetR(double r);
+
+    double GetA();
+    int SetA(double a);
+
+    double GetB();
+    int SetB(double b);
+
+    double GetM();
+    int SetM(double m);
+
+    double GetK();
+    int SetK(double k);
+
+    double GetL();
+    int SetL(double l);
+
+    double GetDt();
+    int SetDt(double d);
+
+    int GetN();
+    int SetN(int n);
+
+    std::string GetFileName();
+    int SetFileName(std::string fileNameStr);
+
+    std::string GetJsonString();
+    int SetJsonString(std::string jsonStr);
     
+private:
+  int CheckSign(double var, std::string varname);
+  int CheckSign(int var, std::string varname);
+  json j;
+  double r; // Hare birth rate.
+  double a; // Predation rate.
+  double b; // Puma birth rate per eaten hare.
+  double m; // Puma mortality rate.
+  double k; // Diffusion rate for hares.
+  double l; // Diffusion rate for pumas.
+  double d; // Time step size.
+  int n; // Number of time steps.
+  std::string fileName;
+  json options;
+  std::string jsonString;
 
 };
 
 #endif /* CONFIGURATIONGENERATOR_HPP */
-
--- src/core/ConfigurationGenerator.cpp
+++ src/core/ConfigurationGenerator.cpp
@@ -4,10 +4,10 @@
  * and open the template in the editor.
  */
 
-/* 
+/*
  * File:   ConfigurationGenerator.cpp
  * Author: alex
- * 
+ *
  * Created on October 9, 2018, 6:43 PM
  */
 
@@ -35,11 +35,11 @@ ConfigurationGenerator::ConfigurationGenerator(int ac, char *av[]) {
     {
         parser.ParseCLI(ac, av);
     }
-    
+
     catch(args::Help){
         std::cout << parser;
     }
-    
+
     catch(args::ParseError e){
         std::cerr << e.what() << std::endl;
         std::cerr << parser;
@@ -49,18 +49,192 @@ ConfigurationGenerator::ConfigurationGenerator(int ac, char *av[]) {
         std::cerr << e.what() << std::endl;
         std::cerr << parser;
     }
-    
-    this->fileName = args::get(filename);
-    this->options["r"] = args::get(r);
-    this->options["a"] = args::get(a);
-    this->options["b"] = args::get(b);
-    this->options["m"] = args::get(m);
-    this->options["k"] = args::get(k);
-    this->options["l"] = args::get(l);
-    this->options["d"] = args::get(d);
-    this->options["n"] = args::get(n);
-    this->jsonString = this->options.dump(4);
+
+
+    this->SetFileName(args::get(filename));
+    this->SetR(args::get(r));
+    this->SetA(args::get(a));
+    this->SetB(args::get(b));
+    this->SetM(args::get(m));
+    this->SetK(args::get(k));
+    this->SetL(args::get(l));
+    this->SetDt(args::get(d));
+    this->SetN(args::get(n));
+    this->SetJsonString(this->options.dump(4));
 }
+
+std::string ConfigurationGenerator::GetFileName()
+{
+  return this->fileName;
+}
+
+int ConfigurationGenerator::SetFileName(std::string fileNameStr)
+{
+  try
+  {
+    this->fileName = fileNameStr;
+    return 0;
+  }
+  catch(const std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+    return -1;
+  }
+}
+
+std::string ConfigurationGenerator::GetJsonString()
+{
+  return this->jsonString;
+};
+
+int ConfigurationGenerator::SetJsonString(std::string jsonStr)
+{
+  try
+  {
+    this->jsonString = jsonStr;
+    return 0;
+  }
+  catch(const std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+    return -1;
+  }
+};
+
+double ConfigurationGenerator::GetR()
+{
+  return this->r;
+};
+
+int ConfigurationGenerator::SetR(double r)
+{
+  this->r = r;
+  return CheckSign(this->r, "r");
+};
+
+
+
+
+double ConfigurationGenerator::GetA()
+{
+  return this->a;
+};
+int ConfigurationGenerator::SetA(double a)
+{
+  this->a = a;
+  return CheckSign(this->a, "a");
+};
+
+
+
+
+double ConfigurationGenerator::GetB()
+{
+  return this->b;
+};
+int ConfigurationGenerator::SetB(double b)
+{
+  this->b = b;
+  return CheckSign(this->b,"b");
+};
+
+
+
+
+double ConfigurationGenerator::GetM()
+{
+  return this->m;
+};
+int ConfigurationGenerator::SetM(double m)
+{
+  this->m = m;
+  return CheckSign(this->m,"m");
+};
+
+
+
+
+
+double ConfigurationGenerator::GetK()
+{
+  return this->k;
+};
+int ConfigurationGenerator::SetK(double k)
+{
+  this->k = k;
+  return CheckSign(this->k, "k");
+};
+
+
+
+
+
+double ConfigurationGenerator::GetL()
+{
+  return this->l;
+};
+int ConfigurationGenerator::SetL(double l)
+{
+  this->l = l;
+  return CheckSign(this->l,"l");
+};
+
+
+
+
+
+double ConfigurationGenerator::GetDt()
+{
+  return this->d;
+};
+int ConfigurationGenerator::SetDt(double d)
+{
+  this->d = d;
+  return CheckSign(this->d, "d");
+};
+
+
+
+
+int ConfigurationGenerator::GetN()
+{
+  return this->n;
+};
+int ConfigurationGenerator::SetN(int n)
+{
+  this->n = n;
+  return CheckSign(this->n, "n");
+};
+
+
+
+
+int ConfigurationGenerator::CheckSign(double var, std::string varname)
+{
+  if(var > 0.0)
+  {
+    return 0;
+  }
+  else
+  {
+    std::cout << std::endl << "Configuration error: variable '" << varname << "' has the wrong sign." << std::endl;
+    return -1;
+  }
+};
+
+int ConfigurationGenerator::CheckSign(int var, std::string varname)
+{
+  if(var > 0)
+  {
+    return 0;
+  }
+  else
+  {
+    std::cout << std::endl << "Configuration error: variable '" << varname << "' has the wrong sign." << std::endl;
+    return -1;
+  }
+
+
 
 bool ConfigurationGenerator::WriteToFile(std::string fileName){
     try
@@ -74,6 +248,5 @@ bool ConfigurationGenerator::WriteToFile(std::string fileName){
         return false;
     }
     return true;
-    
-};
 
+};

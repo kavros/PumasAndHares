a6c7d826ff92c8830d0d471c6b5a47ad36b5e497
kavros <akavroulakis@gmail.com>
2018-11-02 09:52:16 +0000

    fix scripts to be consistent

--- scripts/generateLandscapes.sh
+++ scripts/generateLandscapes.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 
-./build/bin/landscapeGeneratorMain ./data/landscapes/10X10_1.dat -l 0.1 -r 10 -c 10
-./build/bin/landscapeGeneratorMain ./data/landscapes/10X10_3.dat -l 0.3 -r 10 -c 10
-./build/bin/landscapeGeneratorMain ./data/landscapes/10X10_7.dat -l 0.7 -r 10 -c 10
-./build/bin/landscapeGeneratorMain ./data/landscapes/10X10_9.dat -l 0.9 -r 10 -c 10
+./build/bin/landscapeGeneratorMain ./data/landscapes/10x10_1.dat -l 0.1 -r 10 -c 10
+./build/bin/landscapeGeneratorMain ./data/landscapes/10x10_3.dat -l 0.3 -r 10 -c 10
+./build/bin/landscapeGeneratorMain ./data/landscapes/10x10_7.dat -l 0.7 -r 10 -c 10
+./build/bin/landscapeGeneratorMain ./data/landscapes/10x10_9.dat -l 0.9 -r 10 -c 10
 
-./build/bin/landscapeGeneratorMain ./data/landscapes/100X100_1.dat -l 0.1 -r 100 -c 100
-./build/bin/landscapeGeneratorMain ./data/landscapes/100X100_3.dat -l 0.3 -r 100 -c 100
-./build/bin/landscapeGeneratorMain ./data/landscapes/100X100_7.dat -l 0.7 -r 100 -c 100
-./build/bin/landscapeGeneratorMain ./data/landscapes/100X100_9.dat -l 0.9 -r 100 -c 100
+./build/bin/landscapeGeneratorMain ./data/landscapes/100x100_1.dat -l 0.1 -r 100 -c 100
+./build/bin/landscapeGeneratorMain ./data/landscapes/100x100_3.dat -l 0.3 -r 100 -c 100
+./build/bin/landscapeGeneratorMain ./data/landscapes/100x100_7.dat -l 0.7 -r 100 -c 100
+./build/bin/landscapeGeneratorMain ./data/landscapes/100x100_9.dat -l 0.9 -r 100 -c 100
 
-./build/bin/landscapeGeneratorMain ./data/landscapes/1000X1000_1.dat -l 0.1 -r 1000 -c 1000
-./build/bin/landscapeGeneratorMain ./data/landscapes/1000X1000_3.dat -l 0.3 -r 1000 -c 1000
-./build/bin/landscapeGeneratorMain ./data/landscapes/1000X1000_5.dat -l 0.5 -r 1000 -c 1000
-./build/bin/landscapeGeneratorMain ./data/landscapes/1000X1000_9.dat -l 0.9 -r 1000 -c 1000
+./build/bin/landscapeGeneratorMain ./data/landscapes/1000x1000_1.dat -l 0.1 -r 1000 -c 1000
+./build/bin/landscapeGeneratorMain ./data/landscapes/1000x1000_3.dat -l 0.3 -r 1000 -c 1000
+./build/bin/landscapeGeneratorMain ./data/landscapes/1000x1000_7.dat -l 0.7 -r 1000 -c 1000
+./build/bin/landscapeGeneratorMain ./data/landscapes/1000x1000_9.dat -l 0.9 -r 1000 -c 1000
 
 ./build/bin/landscapeGeneratorMain ./data/landscapes/2000x2000_1.dat -l 0.1 -r 2000 -c 2000
 ./build/bin/landscapeGeneratorMain ./data/landscapes/2000x2000_3.dat -l 0.3 -r 2000 -c 2000
--- scripts/validateSimulations.sh
+++ scripts/validateSimulations.sh
@@ -3,91 +3,90 @@ for i in {1..9}
 do
 python  ./scripts/validation.py\
 		"./data/outputs/10x10_1_config${i}__averages.txt" \
-		./data/landscapes/10X10_1.dat\
+		./data/landscapes/10x10_1.dat\
 		"./data/outputs/10x10_1_config${i}_0.ppm"
 
 python  ./scripts/validation.py\
 		"./data/outputs/10x10_3_config${i}__averages.txt" \
-		./data/landscapes/10X10_3.dat\
+		./data/landscapes/10x10_3.dat\
 		./data/outputs/10x10_3_config${i}_0.ppm
 
 python  ./scripts/validation.py\
 		"./data/outputs/10x10_7_config${i}__averages.txt" \
-		./data/landscapes/10X10_7.dat\
+		./data/landscapes/10x10_7.dat\
 		"./data/outputs/10x10_7_config${i}_0.ppm"
 
 
 python  ./scripts/validation.py\
 		"./data/outputs/10x10_9_config${i}__averages.txt" \
-		./data/landscapes/10X10_9.dat\
+		./data/landscapes/10x10_9.dat\
 		"./data/outputs/10x10_9_config${i}_0.ppm"
 
 ###########
 python  ./scripts/validation.py\
 		"./data/outputs/100x100_1_config${i}__averages.txt" \
-		./data/landscapes/100X100_1.dat\
+		./data/landscapes/100x100_1.dat\
 		"./data/outputs/100x100_1_config${i}_0.ppm"
 
 python  ./scripts/validation.py\
 		"./data/outputs/100x100_3_config${i}__averages.txt" \
-		./data/landscapes/100X100_3.dat\
+		./data/landscapes/100x100_3.dat\
 		"./data/outputs/100x100_3_config${i}_0.ppm"
 
 python  ./scripts/validation.py\
 		"./data/outputs/100x100_7_config${i}__averages.txt" \
-		./data/landscapes/100X100_7.dat\
+		./data/landscapes/100x100_7.dat\
 		"./data/outputs/100x100_7_config${i}_0.ppm"
 
 
 python  ./scripts/validation.py\
 		"./data/outputs/100x100_9_config${i}__averages.txt" \
-		./data/landscapes/100X100_9.dat\
+		./data/landscapes/100x100_9.dat\
 		"./data/outputs/100x100_9_config${i}_0.ppm"
 
 ##############
-#
-#python  ./scripts/validation.py\
-#		"./data/outputs/1000x1000_1_config${i}__averages.txt" \
-#		./data/landscapes/1000X1000_1.dat\
-#		"./data/outputs/1000x1000_1_config${i}_0.ppm"
-#
-#python  ./scripts/validation.py\
-#		"./data/outputs/1000x1000_3_config${i}__averages.txt" \
-#		./data/landscapes/1000X1000_3.dat\
-#		"./data/outputs/1000x1000_3_config${i}_0.ppm"
-#
-#python  ./scripts/validation.py\
-#		"./data/outputs/1000x1000_7_config${i}__averages.txt" \
-#		./data/landscapes/1000X1000_7.dat\
-#		"./data/outputs/1000x1000_7_config${i}_0.ppm"
-#
-#
-#python  ./scripts/validation.py\
-#		"./data/outputs/1000x1000_9_config${i}__averages.txt" \
-#		./data/landscapes/1000X1000_9.dat\
-#		"./data/outputs/1000x1000_9_config${i}_0.ppm"
-#
-#################
-#
-#python  ./scripts/validation.py\
-#		"./data/outputs/2000x2000_1_config${i}__averages.txt" \
-#		./data/landscapes/2000X2000_1.dat\
-#		"./data/outputs/2000x2000_1_config${i}_0.ppm"
-#
-#python  ./scripts/validation.py\
-#		"./data/outputs/2000x2000_3_config${i}__averages.txt" \
-#		./data/landscapes/2000X2000_3.dat\
-#		"./data/outputs/2000x2000_3_config${i}_0.ppm"
-#
-#python  ./scripts/validation.py\
-#		"./data/outputs/2000x2000_7_config${i}__averages.txt" \
-#		./data/landscapes/2000X2000_7.dat\
-#		"./data/outputs/2000x2000_7_config${i}_0.ppm"
-#
-#
-#python  ./scripts/validation.py\
-#		"./data/outputs/2000x2000_9_config${i}__averages.txt" \
-#		./data/landscapes/2000X2000_9.dat\
-#		"./data/outputs/2000x2000_9_config${i}_0.ppm"
-#
+python  ./scripts/validation.py\
+		"./data/outputs/1000x1000_1_config${i}__averages.txt" \
+		./data/landscapes/1000x1000_1.dat\
+		"./data/outputs/1000x1000_1_config${i}_0.ppm"
+
+python  ./scripts/validation.py\
+		"./data/outputs/1000x1000_3_config${i}__averages.txt" \
+		./data/landscapes/1000x1000_3.dat\
+		"./data/outputs/1000x1000_3_config${i}_0.ppm"
+
+python  ./scripts/validation.py\
+		"./data/outputs/1000x1000_7_config${i}__averages.txt" \
+		./data/landscapes/1000x1000_7.dat\
+		"./data/outputs/1000x1000_7_config${i}_0.ppm"
+
+
+python  ./scripts/validation.py\
+		"./data/outputs/1000x1000_9_config${i}__averages.txt" \
+		./data/landscapes/1000x1000_9.dat\
+		"./data/outputs/1000x1000_9_config${i}_0.ppm"
+
+################
+
+python  ./scripts/validation.py\
+		"./data/outputs/2000x2000_1_config${i}__averages.txt" \
+		./data/landscapes/2000x2000_1.dat\
+		"./data/outputs/2000x2000_1_config${i}_0.ppm"
+
+python  ./scripts/validation.py\
+		"./data/outputs/2000x2000_3_config${i}__averages.txt" \
+		./data/landscapes/2000x2000_3.dat\
+		"./data/outputs/2000x2000_3_config${i}_0.ppm"
+
+python  ./scripts/validation.py\
+		"./data/outputs/2000x2000_7_config${i}__averages.txt" \
+		./data/landscapes/2000x2000_7.dat\
+		"./data/outputs/2000x2000_7_config${i}_0.ppm"
+
+
+python  ./scripts/validation.py\
+		"./data/outputs/2000x2000_9_config${i}__averages.txt" \
+		./data/landscapes/2000x2000_9.dat\
+		"./data/outputs/2000x2000_9_config${i}_0.ppm"
+
 done
